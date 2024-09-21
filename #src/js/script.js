26c9fad9833055c419e8ac93ec3072fc67e5410b
svone <serjvolk222@gmail.com>
2024-09-21 04:12:01 +0300

    FAQ

--- #src/js/script.js
+++ #src/js/script.js
@@ -612,6 +612,28 @@ $(document).ready(function () {
             }
         }, 1000);
     })();
+
+    // Выпадающие списки
+    (function () {
+        // Клик по выпадающему списку
+        $(document).on("click", ".js-dropdown-head", function(){
+            if($(this).closest(".js-dropdown-item").hasClass('js-only-md4') && w > BREAKPOINT_md4){ return; }
+
+            let dropdownItem = $(this).closest('.js-dropdown-item');
+            let dropdownBody = dropdownItem.find('.js-dropdown-body');
+            let isActive = dropdownItem.hasClass('active');
+            dropdownItem.toggleClass("active", !isActive);
+            isActive ? dropdownBody.slideUp(300) : dropdownBody.slideDown(300);
+        });
+
+        /** При инициализации страницы закрываем/открываем выпадающий список в зависимости от класса "active" */
+        $(".js-dropdown-item").each(function(){
+            if($(this).hasClass('js-only-md4') && w > BREAKPOINT_md4){ return; }
+
+            let isActive = $(this).hasClass('active');
+            isActive ? $(this).find('.js-dropdown-body').show() : $(this).find('.js-dropdown-body').hide();
+        });
+    })();
     
 /** ======================================================================== */
 
